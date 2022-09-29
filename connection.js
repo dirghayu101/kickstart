0d1955b28daf097e2ce5d7bbfecf970a95520621
Dirghayu Joshi <dirghayujoshi@Dirghayus-MacBook-Air.local>
2022-09-29 09:49:40 +0530

    Added more frontend and a simple DB integration with create command.

--- connection.js
+++ connection.js
@@ -1,11 +1,20 @@
-const {Client} = require('pg');
+const {Client} = require('pg')
 
 const client = new Client({
-  host: 'localhost',
-  user: 'postgres',
-  port: 5432,
-  password: 'rootUser',
-  database: 'postgres'
-}); 
-
+    host: 'localhost',
+    user: 'postgres',
+    port: 5432,
+    password: '618903',
+    database: 'KickStart-WorkHub'
+  }); 
+  
+/*client.connect();
+client.query(`Select * FROM public."User";`, (err, res) =>{
+  if(!err){
+    console.log(res.rows);
+  } else{
+    console.log(err.message);
+  }
+  client.end;
+}) */
 module.exports = client;
---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser'); 
+const app = express();
+const bcrypt =  require('bcryptjs')
+const client = require('./connection');
+
+app.use(express.static('./frontend'))
+app.use(bodyParser.json())
+
+
+
+app.get('/', (req, res) => {
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, './frontend/home.html'));
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './frontend/sign-up.html'));
+
+});
+
+app.get('/login', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './frontend/login.html'));
+});
+client.connect();
+app.post('/api/signup', async (req, res) =>{
+    console.log(req.body);
+    const {password: plainTextPassword, firstName, lastName, phoneNumber, emailID, gender} = req.body;
+    const password = await bcrypt.hash(plainTextPassword, 10);
+    try{
+        var insertQuery = `INSERT INTO public."User"(
+            "fName", "lName", "phoneNumber", password, "eMail", gender)
+            VALUES ('${firstName}', '${lastName}', ${phoneNumber}, '${password}', '${emailID}', 'Male' )`;
+        
+        console.log(insertQuery);
+        client.query(insertQuery, (err, result) => {
+            if(!err){
+                console.log("Insertion was successful!");
+            }
+            else{
+                var error = err;
+                throw error;
+            }
+            client.end;
+        });
+    }
+    catch(error){
+        if(error.code === 23505){
+            return res.json({status: 'error', error: 'Mobile Number or email address already exist.'});
+        }
+        else{
+            throw error;
+        }
+
+    }
+    res.json({status: 'ok'});
+});
+
+app.listen(3000, () => {
+    
+    console.log(path.resolve(__dirname, './frontend/home.html'));
+    console.log('server is listening on port 3000....')
+})
